be528f728828f72484770b7b022d2ac0b6289663
yaseenmirshal <151372491+yaseenmirshal@users.noreply.github.com>
2025-03-08 09:24:01 +0400

    Resolved merge conflicts

--- app/page.js
+++ app/page.js
@@ -1,14 +1,24 @@
 import Image from "next/image";
 import Landing from "./Components/Landing";
 import  Warning  from "./Components/Warning";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+
 
 
 
 export default function Home() {
   return (
    <>
+
    {/* <Landing/> */}
    <Warning/>
+
+   <Navbar/>
+   <Landing/>
+   <Footer/>
+   {/* <Warning/> */}
+
 
  
    </>
